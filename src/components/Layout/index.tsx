0140d3477e3dc17648fee16666fa33836fa224df
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-07-22 20:44:30 -0300

    Change Layout component to a folder to fix import

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
-import Header from "./Header/header"
-import Footer from "./Footer/footer"
+import Footer from "../Footer/footer"
+import Header from "../Header/header"
 
 interface LayoutProps {
   children: React.ReactNode
