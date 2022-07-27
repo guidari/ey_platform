fe3481d774bb5edbdca9583116f299772f106b9e
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-07-26 22:43:42 -0300

    GlobalRank showing results without filter

--- src/pages/challenges/index.tsx
+++ src/pages/challenges/index.tsx
@@ -6,15 +6,46 @@ import { collection, DocumentData, getDocs } from "firebase/firestore"
 import { db } from "../../config/firebase"
 import { useEffect, useState } from "react"
 import Spinner from "../../components/Spinner"
+import { IUser } from "../../interface/IUser"
 
 export default function Page() {
   const [users, setUsers] = useState<DocumentData>()
 
   async function getAllUsers() {
     const docRef = collection(db, "users")
-    console.log("docRef", docRef)
+    // console.log("docRef", docRef)
     const docSnap = await getDocs(docRef)
+    console.log("docSnap", docSnap)
 
+    let globalRank = ""
+
+    if (docSnap.size > 0) {
+      docSnap.forEach((item) => {
+        globalRank += `
+        <div class="flex gap-3 mb-5">
+          <div>
+            <img src=${
+              item.data().image == ""
+                ? `/images/userGeneric.png`
+                : item.data().image
+            } alt="User" class="rounded-full w-12" />
+          </div>
+    
+          <div>
+            <h3 class="text-md font-semibold">${item.data().name}</h3>
+            <p class="text-sm">
+              Solved challenges:
+              <span class="text-yellow-1"> ${
+                item.data().progress.challenges
+              }</span>
+            </p>
+          </div>
+        </div>`
+      })
+      document.querySelector("#globalRank")!.innerHTML = globalRank
+    } else {
+      document.querySelector("#globalRank")!.innerHTML = "No users found"
+    }
     setUsers(docSnap)
   }
 
@@ -30,29 +61,8 @@ export default function Page() {
 
           <div className="flex-initial w-3/6 maxxl:w-full p-5 bg-gray-1 rounded-md mt-5">
             <h1 className="text-xl font-semibold mb-5">Global Rank 🏆</h1>
-            <UserRank name="Sana Minatozaki" solvedChallenges={14} />
-
-            {!users ? (
-              <Spinner />
-            ) : (
-              users.forEach((item: any) => {
-                // <UserRank
-                //   name={item.data().name}
-                //   solvedChallenges={item.data().progress.challenges}
-                // />
-
-                return item.data().name
-              })
-            )}
-
-            {!users ? (
-              <Spinner />
-            ) : (
-              console.log(
-                "docSnap",
-                users?.forEach((item: any) => console.log(item.data()))
-              )
-            )}
+            {/* <UserRank name="Sana Minatozaki" solvedChallenges={14} /> */}
+            <div id="globalRank"></div>
           </div>
         </div>
       </div>
